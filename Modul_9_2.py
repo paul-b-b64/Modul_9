054bbc300f672480258105433c44a87ac7cb73a3
paul-b-b64 <belousko@inbox.ru>
2024-08-20 18:36:03 +0300

    9.2

--- Modul_9_2.py
+++ Modul_9_2.py
@@ -5,8 +5,6 @@ first_result = [len(x) for x in first_strings if len(x) > 5]
 second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
 third_result = [{x: len(x)} for x in first_strings + second_strings if not len(x) % 2]
 
-
-
 print(first_result)
 print(second_result)
 print(third_result)

c2893bcd07bb55e96d18cd0cbabc66a44fcf6640
paul-b-b64 <belousko@inbox.ru>
2024-08-23 15:15:22 +0300

    9.4

---
+++ Modul_9_4.py
@@ -0,0 +1,43 @@
+# Lambda-функция:
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+# Замыкание:
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for i in data_set:
+                if isinstance(i, str):
+                    file.write(f'{i}\n')
+                else:
+                    string_ = str(i)
+                    file.write(f'{string_}\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+# Метод __call__:
+
+from random import choice
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
